4534e635c8510516ba28fe727831683d7cb94056
Elian-Tester <eliancruz01@gmail.com>
2022-06-18 19:16:43 -0500

    feature/006 vista signup terminada

--- lib/src/pages/signup.dart
+++ lib/src/pages/signup.dart
@@ -11,88 +11,232 @@ class signup extends StatefulWidget {
 class _signupState extends State<signup> {
   final GlobalKey<FormState> _formKey = GlobalKey();
 
-  // This function is triggered when the "Save" button is pressed
   void _saveForm() {
     final bool isValid = _formKey.currentState!.validate();
     if (isValid) {
       if (kDebugMode) {
-        print('Got a valid input');
+        print('Valido');
       }
-      // And do something here
     }
   }
 
+  bool value = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Kindacode.com'),
+        title: titulo(),
+        backgroundColor: Colors.deepPurple,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
+      body: SingleChildScrollView(child: formulario()),
+    );
+  }
+
+  Widget titulo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [const Text('Regístrate'), logo()],
+    );
+  }
+
+  Widget logo() {
+    return Image.asset(
+      'assets/B5.png',
+      fit: BoxFit.contain,
+      height: 60,
+    );
+  }
+
+  Widget formulario() {
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: form(),
+    );
+  }
+
+  Widget form() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          etiquetaTexto("Crea una cuenta para empezar a usar la app", 15.0,
+              FontWeight.bold, Colors.black54),
+          const SizedBox(
+            height: 38,
+          ),
+          etiquetaIzquierda("Nombre"),
+          campoNombre(),
+          const SizedBox(
+            height: 25,
+          ),
+          etiquetaIzquierda("Correo electrónico"),
+          campoCorreo(),
+          const SizedBox(
+            height: 25,
+          ),
+          etiquetaIzquierda("Contraseña"),
+          campoPassword(),
+          Container(
+            child: etiquetaTexto(
+                "La contraseña debe contener caracteres, números y símboloes \n con u minimo de 6 caracteres.",
+                12.0,
+                FontWeight.normal,
+                Colors.black26),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          Row(
             children: [
-              TextFormField(
-                validator: (value) {
-                  if (value != null && value.trim().length < 3) {
-                    return 'This field requires a minimum of 3 characters';
-                  }
-
-                  return null;
-                },
-                decoration: const InputDecoration(
-                    labelText: 'Nombre completo',
-                    border: OutlineInputBorder(),
-                    errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red, width: 5))),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value != null && value.trim().length < 3) {
-                    return 'This field requires a minimum of 3 characters';
-                  }
-
-                  return null;
-                },
-                decoration: const InputDecoration(
-                    labelText: 'Direccion de correo',
-                    border: OutlineInputBorder(),
-                    errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red, width: 5))),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value != null && value.trim().length < 3) {
-                    return 'This field requires a minimum of 3 characters';
-                  }
-
-                  return null;
-                },
-                decoration: const InputDecoration(
-                    labelText: 'Contraseña',
-                    border: OutlineInputBorder(),
-                    errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red, width: 5))),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              ElevatedButton.icon(
-                  onPressed: _saveForm,
-                  icon: const Icon(Icons.save),
-                  label: const Text('Save'))
+              checkBox(),
+              terminosCondiciones(),
             ],
           ),
-        ),
+          const SizedBox(
+            height: 0,
+          ),
+          botonCrearCuenta(),
+          Center(
+            child: tienesCuenta(),
+          )
+        ],
       ),
+    );
+  }
+
+  Widget campoNombre() {
+    return TextFormField(
+      validator: (value) {},
+      decoration: const InputDecoration(
+          hintText: 'Nombre completo',
+          hintStyle: TextStyle(color: Colors.black26),
+          border: OutlineInputBorder(),
+          errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red, width: 5))),
+    );
+  }
+
+  Widget campoCorreo() {
+    return TextFormField(
+      validator: (value) {},
+      decoration: const InputDecoration(
+          hintText: 'Direccion de correo',
+          hintStyle: TextStyle(color: Colors.black26),
+          border: OutlineInputBorder(),
+          errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red, width: 5))),
+    );
+  }
+
+  Widget campoPassword() {
+    return TextFormField(
+      validator: (value) {},
+      decoration: const InputDecoration(
+          hintText: 'Contraseña',
+          hintStyle: TextStyle(color: Colors.black26),
+          border: OutlineInputBorder(),
+          errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red, width: 5))),
+    );
+  }
+
+  Widget botonCrearCuenta() {
+    return ElevatedButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+            primary: Colors.white,
+            backgroundColor: Colors.green,
+            elevation: 3,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0)),
+            minimumSize: Size(300, 50)),
+        child: Text(
+          "Crear Cuenta",
+          style: TextStyle(fontSize: 18),
+        ));
+  }
+
+  Widget etiquetaIzquierda(texto) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          texto,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+        )
+      ],
+    );
+  }
+
+  Widget etiquetaTexto(texto, fontSize, fontWeight, colors) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          texto,
+          style: TextStyle(
+              fontWeight: fontWeight, fontSize: fontSize, color: colors),
+        )
+      ],
+    );
+  }
+
+  Widget terminosCondiciones() {
+    return Container(
+      width: 300,
+      alignment: Alignment.centerLeft,
+      child: msjTerminosCondiciones(),
+    );
+  }
+
+  Widget tienesCuenta() {
+    double fontSize = 15.0;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        etiquetaTexto("¿Ya tienes una cuenta?", fontSize, FontWeight.normal,
+            Colors.black),
+        TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/');
+            },
+            child: etiquetaTexto(
+                "Inicia sesión ", fontSize, FontWeight.bold, Colors.red)),
+      ],
+    );
+  }
+
+  Widget checkBox() {
+    return Checkbox(
+      value: this.value,
+      onChanged: (bool? value) {
+        setState(() {
+          this.value = value!;
+        });
+      },
+    );
+  }
+
+  Widget msjTerminosCondiciones() {
+    double fontSize = 13.0;
+    return Column(
+      children: [
+        Row(
+          children: [
+            etiquetaTexto("Al registrarme, acepto los ", fontSize,
+                FontWeight.normal, Colors.black),
+            etiquetaTexto("términos y condicionbes ", fontSize,
+                FontWeight.normal, Colors.red),
+          ],
+        ),
+        Row(
+          children: [
+            etiquetaTexto("y la ", fontSize, FontWeight.normal, Colors.black),
+            etiquetaTexto("policica de privacidad ", fontSize,
+                FontWeight.normal, Colors.red),
+          ],
+        )
+      ],
     );
   }
 }
